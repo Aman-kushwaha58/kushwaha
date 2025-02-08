5ee96439e70680ce9ef355d198f084b24add618e
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-02-08 17:15:27 +0530

    Add files via upload

---
+++ resulting_data.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class resulting_data {
+
+    public static void main(String[] args) {
+        int a = 10;
+        float y = 6.5f + a;
+        System.out.println(y);
+     {
+}
